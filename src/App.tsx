86d02fa21cf9fc7d783d0a39409b6ebadc457386
xujiangzhe <xujiangzhe1997@gmail.com>
2024-09-16 19:09:19 -0700

    make kanbas default

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Kanbas from './Kanbas';
 import Labs from './Labs';
-import { HashRouter, Link, Route, Routes } from 'react-router-dom';
+import { HashRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <HashRouter>
       <div>
-        <Link to="/Labs">Labs</Link> | <Link to="/Labs/Lab1">Lab1</Link> | 
-        <Link to="/Labs/Lab2">Lab2</Link> | <Link to="/Labs/Lab3">Lab3</Link> |
-        <Link to="/Kanbas">Kanbas</Link>
         <Routes>
+          <Route path="/" element={<Navigate to="/Kanbas" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
