6d6c26bb2151cf9c5f410c411e1304ed2b8ffea4
xujiangzhe <xujiangzhe1997@gmail.com>
2024-11-06 17:37:44 -0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import store from "./Kanbas/store";
+import { Provider } from "react-redux";
 import Labs from "./Labs";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Kanbas from "./Kanbas";
 export default function App() {
  return (
   <HashRouter>
-   <div>
-    <Routes>
-     <Route path="/" element={<Navigate to="Kanbas"/>}/>
-     <Route path="/Labs/*" element={<Labs />} />
-     <Route path="/Kanbas/*" element={<Kanbas />} />
-    </Routes>
-   </div>
+    <Provider store={store}>
+      <div>
+        <Routes>
+        <Route path="/" element={<Navigate to="Kanbas"/>}/>
+        <Route path="/Labs/*" element={<Labs />} />
+        <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </Provider>
   </HashRouter>
 );}
