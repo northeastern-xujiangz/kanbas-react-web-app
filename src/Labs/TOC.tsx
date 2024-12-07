9b7aa8be0754e63f545a803891b8b56430668aea
Jiangzhe Xu <157778649+northeastern-xujiangz@users.noreply.github.com>
2024-12-06 17:44:58 -0800

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -44,7 +44,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-github" href="https://github.com/1tou" className="nav-link">
+        <a id="wd-github" href="https://github.com/northeastern-xujiangz" className="nav-link">
           My GitHub
         </a>
       </li>
